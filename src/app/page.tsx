fee273c974b5846a4575c5762e138ea15f55b12d
nosukeuehara <ryuunosuke06160616@gmail.com>
2024-08-04 22:04:23 +0900

    update : ページの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+"use client"
+
 import Login from "@/components/login";
+import { signIn } from "next-auth/react";
 
 export type User = {
   email: string,
   name: string
 }
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className=" flex justify-center items-center align-middle h-14">
       <form>
         <Login />
       </form>
+      <button onClick={() => signIn()}>
+        sign in
+      </button>
     </div>
   );
 }
