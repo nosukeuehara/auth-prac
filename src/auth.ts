117085ead25b264bb8a42b09aeef2b1b2008602d
DE-Uehara <r.uehara@defide-ix.com>
2024-08-08 10:04:18 +0900

    update : authの設定改修

--- src/auth.ts
+++ src/auth.ts
@@ -3,36 +3,39 @@ import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { prisma } from "./prisma";
+import { Provider } from "next-auth/providers";
+
+const providers: Provider[] = [
+  Google,
+  Github,
+  Credentials({
+    credentials: {
+      email: { label: "email", type: "email" },
+      password: { label: "Password", type: "password" },
+    },
+    authorize: async (credentials) => {
+      if (!credentials?.email || !credentials.password) {
+        return null;
+      }
+      // データベースからデータを取得して比較するロジックが必要
+      prisma.$connect()
+      const user = await prisma.user.findUnique({
+        where: { email: credentials.email as string }
+      })
+      prisma.$disconnect()
+      if (user === null) {
+        throw new Error(" user not found ");
+      }
+      return {
+        name: user.name,
+        email: user.email
+      };
+    },
+  })
+]
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google,
-    Github,
-    Credentials({
-      credentials: {
-        email: { label: "email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
-        // データベースからデータを取得して比較するロジックが必要
-        prisma.$connect()
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email as string }
-        })
-        prisma.$disconnect()
-        if (user === null) {
-          throw new Error(" user not found ");
-        }
-        return {
-          name: user.name,
-          email: user.email
-        };
-      },
-    }),
-  ],
+  providers,
   callbacks: {
     async redirect({ url, baseUrl }) {
       // 認証後にリダイレクトしたいURLを指定します
@@ -40,3 +43,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider()
+    return { id: providerData.id, name: providerData.name }
+  } else {
+    return { id: provider.id, name: provider.name }
+  }
+})
