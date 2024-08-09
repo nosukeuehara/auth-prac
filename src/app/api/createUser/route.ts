7a76f4716b026cc1f40562676ccf6ce517b46d55
DE-Uehara <r.uehara@defide-ix.com>
2024-08-09 16:04:08 +0900

    update : パスワードのハッシュ処理追加

--- src/app/api/createUser/route.ts
+++ src/app/api/createUser/route.ts
@@ -1,15 +1,19 @@
 import { prisma } from "@/prisma";
 import { NextResponse } from "next/server";
+import bcrypt from 'bcrypt';
 
 export async function POST(request: Request, response: Response) {
+  const { email, name, password } = await request.json();
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
   try {
     prisma?.$connect();
-    const { email, name, password } = await request.json();
     const allUser = await prisma?.user.create({
       data: {
         email,
         name,
-        password
+        password: hash
       },
     });
     return NextResponse.json(
